34a93bdefc2bb48f855ddc3a677fed8793f364e8
az-iddouch <abdelaziz.iddouch@gmail.com>
2019-02-28 14:48:25 +0100

    [server] got token from google setup

--- server/index.js
+++ server/index.js
@@ -1,13 +1,39 @@
 const express = require('express');
 const passport = require('passport');
 const googleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+const morgan = require('morgan');
 
 const app = express();
 
-passport.use(new googleStrategy)
+// for output in console
+app.use(morgan('dev'));
 
+passport.use(
+  new googleStrategy(
+    {
+      clientID: keys.googleClientId,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token :', accessToken);
+      console.log('refresh Token :', refreshToken);
+      console.log('profile :', profile);
+    }
+  )
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT);
-
-
+app.listen(PORT, () => {
+  console.log(`listenning on port ${PORT} ...`);
+});
