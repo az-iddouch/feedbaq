0c87c8a327167b4b9caafbc1a54dc1302f89aa3c
abdelaziz <abdelaziz.iddouch@gmail.com>
2019-03-19 22:42:43 +0100

    setup the login front end flow, show dynamic content on header depending on the auth state ...

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+
 import Header from './Header';
 
 const dashboard = () => <div>dashboard</div>;
 const surveyNew = () => <div>survey new </div>;
 const landing = () => <div>landing </div>;
 
-const App = () => {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={landing} />
-          <Route exact path="/surveys" component={dashboard} />
-          <Route path="/surveys/new" component={surveyNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={landing} />
+            <Route exact path="/surveys" component={dashboard} />
+            <Route path="/surveys/new" component={surveyNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  actions
+)(App);
